ee4397db7caf0d3930178ef4b7283f40430c4676
poipoiyo <48159287+poipoiyo@users.noreply.github.com>
2023-02-09 23:36:33 +0800

    remove useless file

--- public/js/map.js
+++
@@ -1,9 +0,0 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("mapDiv"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-                        
